d680113234d3405f5d2f3a31328dfe62d45977e1
Sho Yamashita <katandps@gmail.com>
2026-01-22 04:52:41 +0900

    fix judgeline offset

--- play/parts/judgeline.lua
+++ play/parts/judgeline.lua
@@ -1,11 +1,12 @@
 local geometry = require("play/geometry").build(27)
+local offsets = require("play/consts").offsets
 
 local function load(skin)
     table.insert(skin.destination, {
         id = -111,
-        offset = 3,
+        offset = offsets.OFFSET_LIFT,
         dst = {
-            { x = geometry.lane_left_margin, y = geometry.lane_under_margin - geometry.note_height / 2, w = geometry.lane_width, h = geometry.note_height / 2, r = 220, g = 0, b = 0 },
+            { x = geometry.lane_left_margin, y = geometry.lane_under_margin - geometry.note_height / 2, w = geometry.lane_width, h = geometry.note_height, r = 80, g = 0, b = 0 },
         }
     })
 end
