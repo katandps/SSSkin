5b90de3c09f3f2697056f8fb7d80c8d9638b9076
Sho Yamashita <katandps@gmail.com>
2026-01-21 16:14:45 +0900

    implement background

---
+++ play/background/default.lua
@@ -0,0 +1,7 @@
+local function load(skin)
+    local header = require("play/header")
+    table.insert(skin.source, { id = "src_background", path = "play/background/default.jpg" })
+    table.insert(skin.image, { id = "background", src = "src_background", x = 0, y = 0, w = header.w, h = header.h })
+    table.insert(skin.destination, { id = "background", dst = { { x = 0, y = 0, w = header.w, h = header.h, a = 64 } } })
+end
+return { load = load }
