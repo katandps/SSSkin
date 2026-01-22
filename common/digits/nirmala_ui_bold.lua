48c9574f5388dce235ab9e1378f962800da4a920
Sho Yamashita <katandps@gmail.com>
2026-01-23 01:05:01 +0900

    fix digits image

--- common/digits/nirmala_ui_bold.lua
+++ common/digits/nirmala_ui_bold.lua
@@ -23,4 +23,29 @@ local function load(skin)
     append_all(skin.source, { { id = "src_number_nirmala_ui_bold", path = "common/digits/nirmala_ui_bold.png" } })
 end
 
-return { load = load, white = white, green = green, yellow = yellow, width = width, height = height }
+--- 数字1個分の画像をimageにして取得
+local function white_image(skin, i)
+    append_all(skin.image, { {
+        id = "nirmala_ui_bold_white_" .. i,
+        src = "src_number_nirmala_ui_bold",
+        x = i * width,
+        y = 0,
+        w = width,
+        h = height,
+    } })
+end
+
+local function white_pick(i)
+    return "nirmala_ui_bold_white_" .. i
+end
+
+return {
+    load = load,
+    white = white,
+    green = green,
+    yellow = yellow,
+    width = width,
+    height = height,
+    white_image = white_image,
+    white_pick = white_pick
+}
