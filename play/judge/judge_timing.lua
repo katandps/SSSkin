916713229c45d5ab76ec338a0031ad77f01e272c
Sho Yamashita <katandps@gmail.com>
2026-01-26 21:14:19 +0900

    十分判定の中心に近いところで押しているとき、白になるように調整

--- play/judge/judge_timing.lua
+++ play/judge/judge_timing.lua
@@ -5,6 +5,21 @@ local value = require("play/consts").value
 local offsets = require("play/consts").offsets
 local timers = require("play/consts").timers
 local geometry = require("play/geometry").build()
+local options = require("play/consts").options
+local main_state = require("main_state")
+
+local function is_precise()
+    local duration = math.abs(main_state.number(value.VALUE_JUDGE_1P_DURATION))
+    return duration < 15
+end
+
+local function is_not_precise()
+    return not is_precise()
+end
+
+local function is_autoplay_off()
+    return main_state.option(options.OPTION_AUTOPLAYOFF)
+end
 
 local function load(skin)
     nirmala_ui_bold.load(skin)
@@ -17,6 +32,12 @@ local function load(skin)
             ref = value.VALUE_JUDGE_1P_DURATION,
             zeropadding = values.zeropadding.NOPADDING
         }),
+        -- 十分正確な場合
+        nirmala_ui_bold.white_integer({
+            id = "judge_timing_precise",
+            digit = 4,
+            ref = value.VALUE_JUDGE_1P_DURATION,
+        }),
     })
 
     local x = geometry.lane_left_margin + geometry.lane_width / 2 - nirmala_ui_bold.width * 2
@@ -26,7 +47,18 @@ local function load(skin)
         {
             id = "judge_timing",
             offsets = { offsets.OFFSET_LIFT, offsets.OFFSET_JUDGE_1P },
-            -- op = { MAIN.OP.AUTOPLAYOFF },
+            loop = -1,
+            timer = timers.TIMER_JUDGE_1P,
+            draw = function() return is_not_precise() and is_autoplay_off() end,
+            dst = {
+                { time = 0,  x = x, y = 420, w = nirmala_ui_bold.width, h = nirmala_ui_bold.height },
+                { time = 500 }
+            }
+        },
+        {
+            id = "judge_timing_precise",
+            offsets = { offsets.OFFSET_LIFT, offsets.OFFSET_JUDGE_1P },
+            draw = function() return is_precise() and is_autoplay_off() end,
             loop = -1,
             timer = timers.TIMER_JUDGE_1P,
             dst = {
