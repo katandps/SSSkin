dcf7e9078fefeec9b6379dd9fbff4cb2bafd621b
Sho Yamashita <katandps@gmail.com>
2026-01-30 14:27:57 +0900

    判定タイミング表示に単位ms表示を追加

--- common/digits/nirmala_ui_bold.lua
+++ common/digits/nirmala_ui_bold.lua
@@ -4,11 +4,30 @@ local append_all = require("utils/append_all")
 local width = 14
 local height = 33
 local divy = 1
-local natural = { src = "src_number_nirmala_ui_bold", divx = 11, divy = divy, w = width * 11, h = height }
-local integer = { src = "src_number_nirmala_ui_bold", divx = 24, divy = divy, w = width * 24, h = height }
+local source_id = "src_number_nirmala_ui_bold"
+local natural = { src = source_id, divx = 11, divy = divy, w = width * 11, h = height }
+local integer = { src = source_id, divx = 24, divy = divy, w = width * 24, h = height }
+local percent_x = 336
+local percent_w = 24
+local period_x = 360
+local period_w = 14
+local ms_x = 374
+local ms_w = 38
 
 local function white(t)
     return merge_recursive(merge_recursive(t, natural), { x = 0, y = 0, })
+end
+
+local function white_percent(t)
+    return merge_recursive(merge_recursive(t, natural), { x = percent_x, y = 0, w = percent_w, })
+end
+
+local function white_period(t)
+    return merge_recursive(merge_recursive(t, natural), { x = period_x, y = 0, w = period_w, })
+end
+
+local function white_ms(t)
+    return merge_recursive(t, { src = source_id, x = ms_x, y = 0, w = ms_w, h = height })
 end
 
 local function white_integer(t)
@@ -32,6 +51,10 @@ local function blue(t)
     return merge_recursive(merge_recursive(t, natural), { x = 0, y = height * 4, })
 end
 
+local function blue_ms(t)
+    return merge_recursive(t, { src = source_id, x = ms_x, y = height * 4, w = ms_w, h = height })
+end
+
 --- マイナスが青 プラスが赤のimageを取得
 local function minus_plus(t)
     return merge_recursive(merge_recursive(t, integer), { x = 0, y = height * 4, })
@@ -39,6 +62,10 @@ end
 
 local function red(t)
     return merge_recursive(merge_recursive(t, natural), { x = 0, y = height * 3, })
+end
+
+local function red_ms(t)
+    return merge_recursive(t, { src = source_id, x = ms_x, y = height * 3, w = ms_w, h = height })
 end
 
 
@@ -69,11 +96,16 @@ return {
     green = green,
     yellow = yellow,
     red = red,
+    red_ms = red_ms,
     blue = blue,
+    blue_ms = blue_ms,
     plus_minus = plus_minus,
     minus_plus = minus_plus,
     width = width,
     height = height,
     white_image = white_image,
     white_pick = white_pick,
+    white_percent = white_percent,
+    white_period = white_period,
+    white_ms = white_ms,
 }
--- play/judge/judge_timing.lua
+++ play/judge/judge_timing.lua
@@ -13,6 +13,16 @@ local function is_precise()
     return duration < 15
 end
 
+local function is_early()
+    local duration = main_state.number(VALUE.VALUE_JUDGE_1P_DURATION)
+    return duration > 0
+end
+
+local function is_late()
+    local duration = main_state.number(VALUE.VALUE_JUDGE_1P_DURATION)
+    return duration < 0
+end
+
 local function is_not_precise()
     return not is_precise()
 end
@@ -39,6 +49,12 @@ local function load(skin)
             ref = VALUE.VALUE_JUDGE_1P_DURATION,
         }),
     })
+    -- ms表示
+    append_all(skin.image, {
+        nirmala_ui_bold.white_ms({ id = "judge_timing_ms_precise", }),
+        nirmala_ui_bold.blue_ms({ id = "judge_timing_ms_early", }),
+        nirmala_ui_bold.red_ms({ id = "judge_timing_ms_late", }),
+    })
 
     local x = geometry.lane_left_margin + geometry.lane_width / 2 - nirmala_ui_bold.width * 2
     local y = geometry.lane_under_margin + geometry.judge_detail_default_y
@@ -57,6 +73,28 @@ local function load(skin)
             }
         },
         {
+            id = "judge_timing_ms_early",
+            offsets = { OFFSETS.OFFSET_LIFT, OFFSETS.OFFSET_JUDGEDETAIL_1P },
+            draw = function() return is_not_precise() and is_early() and is_autoplay_off() end,
+            loop = -1,
+            timer = TIMERS.TIMER_JUDGE_1P,
+            dst = {
+                { time = 0,  x = x + nirmala_ui_bold.width * 4, y = y, w = 38, h = nirmala_ui_bold.height },
+                { time = 500 }
+            }
+        },
+        {
+            id = "judge_timing_ms_late",
+            offsets = { OFFSETS.OFFSET_LIFT, OFFSETS.OFFSET_JUDGEDETAIL_1P },
+            draw = function() return is_not_precise() and is_late() and is_autoplay_off() end,
+            loop = -1,
+            timer = TIMERS.TIMER_JUDGE_1P,
+            dst = {
+                { time = 0,  x = x + nirmala_ui_bold.width * 4, y = y, w = 38, h = nirmala_ui_bold.height },
+                { time = 500 }
+            }
+        },
+        {
             id = "judge_timing_precise",
             offsets = { OFFSETS.OFFSET_LIFT, OFFSETS.OFFSET_JUDGEDETAIL_1P },
             draw = function() return is_precise() and is_autoplay_off() end,
@@ -64,6 +102,17 @@ local function load(skin)
             timer = TIMERS.TIMER_JUDGE_1P,
             dst = {
                 { time = 0,  x = x, y = y, w = nirmala_ui_bold.width, h = nirmala_ui_bold.height, r = 96, g = 96, b = 96 },
+                { time = 500 }
+            }
+        },
+        {
+            id = "judge_timing_ms_precise",
+            offsets = { OFFSETS.OFFSET_LIFT, OFFSETS.OFFSET_JUDGEDETAIL_1P },
+            draw = function() return is_precise() and is_autoplay_off() end,
+            loop = -1,
+            timer = TIMERS.TIMER_JUDGE_1P,
+            dst = {
+                { time = 0,  x = x + nirmala_ui_bold.width * 4, y = y, w = 38, h = nirmala_ui_bold.height, r = 96, g = 96, b = 96 },
                 { time = 500 }
             }
         },
