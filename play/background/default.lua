a0fc2767cd70e78f4e952271cba0dae6b451cc8d
Sho Yamashita <katandps@gmail.com>
2026-02-09 10:27:52 +0900

    move background resource folder

--- play/background/default.lua
+++ play/background/default.lua
@@ -1,6 +1,6 @@
 local function load(skin)
     local header = require("play/header")
-    table.insert(skin.source, { id = "src_background", path = "play/background/default.jpg" })
+    table.insert(skin.source, { id = "src_background", path = "customize/background/default.jpg" })
     table.insert(skin.image, { id = "background", src = "src_background", x = 0, y = 0, w = header.w, h = header.h })
     table.insert(skin.destination, { id = "background", dst = { { x = 0, y = 0, w = header.w, h = header.h, a = 64 } } })
 end
