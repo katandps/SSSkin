8e7755ad3e964293ff58a891ff85d141e49791c1
Sho Yamashita <katandps@gmail.com>
2026-02-11 02:13:27 +0900

    refactoring

--- utils/load.lua
+++ utils/load.lua
@@ -1,26 +1,36 @@
---- スキンのロード
-
 local append_all = require("utils/append_all")
+
+--- 配列として扱う要素の一覧
+--- @see https://github.com/exch-bms2/beatoraja/blob/17c57c39b9a714ef4b2040100bc0726a04b9ce2a/src/bms/player/beatoraja/skin/json/JsonSkinSerializer.java#L68
+local arrays = {
+    property = 1,
+    filepath = 1,
+    offset = 1,
+    source = 1,
+    font = 1,
+    image = 1,
+    imageset = 1,
+    value = 1,
+    float_value = 1,
+    text = 1,
+    slider = 1,
+    graph = 1,
+    gauge_graph = 1,
+    judge_graph = 1,
+    hit_error_visualizer = 1,
+    timing_visualizer = 1,
+    timing_distribution_graph = 1,
+    judge = 1,
+    destination = 1,
+    animation = 1,
+    custom_event = 1,
+    custom_timer = 1,
+}
 
 --- targetにsourceの内容を追記する
 ---
 --- targetの各要素に対して、sourceの対応する要素を追加する
 --- targetの要素が配列のものは追記、連想配列のものは上書きする
-
---- 配列として扱う要素の一覧
-local arrays = {
-    source = 1,
-    image = 1,
-    font = 1,
-    imageset = 1,
-    text = 1,
-    slider = 1,
-    value = 1,
-    destination = 1,
-    property = 1,
-    filepath = 1,
-    offset = 1
-}
 return function(target, source, context)
     for k, v in pairs(source) do
         if arrays[k] then
