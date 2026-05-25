e0c602e6ce35251f9943b03e6091f610958eebff
Sho Yamashita <katandps@gmail.com>
2026-05-25 14:42:35 +0900

    ボムがリフトに追従しないバグを修正

--- play/parts/bomb/ox.lua
+++ play/parts/bomb/ox.lua
@@ -4,6 +4,7 @@
 local log = require("utils/log")
 local serialize_value = require("utils/serialize_value")
 local TIMERS = require("play/consts").TIMERS
+local OFFSETS = require("play/consts").OFFSETS
 local geometry = require("play/geometry").build()
 local lane_x_positions = geometry.lane_x_positions
 local lane_each_width = geometry.lane_each_width
@@ -59,6 +60,7 @@ for key = 1, 8 do
     table.insert(skin.destination, {
         id = set_bomb_id,
         timer = o_timer_list[key],
+        offsets = { OFFSETS.OFFSET_LIFT },
         loop = -1,
         dst = {
             { time = 0,   x = geometry.lane_left_margin + lane_x_positions[key] + lane_each_width[key] / 2 - bomb_width / 2, y = geometry.judge_line_y - bomb_height / 2 + geometry.note_height / 2, w = bomb_width, h = bomb_height, a = 0 },
