d9baa0982f3c97aae5a5a7b25bc80a68704b8aa5
Sho Yamashita <katandps@gmail.com>
2026-05-28 10:11:58 +0900

    判定ラインの表示位置調整

--- play/parts/judgeline/consts.lua
+++ play/parts/judgeline/consts.lua
@@ -3,6 +3,6 @@ local geometry = require("play/geometry").build()
 
 return {
     offsets = { OFFSETS.OFFSET_NOTES_1P, OFFSETS.OFFSET_LIFT },
-    y_position = geometry.judge_line_y - geometry.note_height,
+    y_position = geometry.judge_line_y,
     NO_LOOP = -1,
 }
